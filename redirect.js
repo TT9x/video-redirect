2a82fd25204ee0fa67b0eb0362b8eb8da9e58181
TT9x <123978112+TT9x@users.noreply.github.com>
2025-07-01 12:13:55 +0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,6 +2,6 @@ const code = window.location.pathname.replace('/', '').trim().toUpperCase();
 if (!code) {
   document.body.innerHTML = "<h2>Thiếu mã vận đơn</h2>";
 } else {
-  const redirectURL = "https://script.google.com/macros/s/AKfycbwe_MTjl2YQ8WtGn5tB3bSgAbXvdbMJpNY5N2cz5ynIREM1LF22sO4sWnFDFpLpiGFJ/exec?code=" + encodeURIComponent(code);
+  const redirectURL = "https://script.google.com/macros/s/AKfycbwIXv5gZ26pzHmKaAHMBOjgAvCPASvVk9_S64EBXmcetnUlTYrES68HhbGvtxuzlyTiQg/exec?code=" + encodeURIComponent(code);
   window.location.replace(redirectURL);
 }
